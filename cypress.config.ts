5024d8bacfee19a0bf4b964e41add39a567271c0
“kaka5277” <254140626@qq.com>
2023-06-15 12:58:07 +1000

    .ts目录只能在support下

---
+++ cypress.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "cypress";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import browserify from "@badeball/cypress-cucumber-preprocessor/browserify";
+
+async function setupNodeEvents(
+    on: Cypress.PluginEvents,
+    config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+    await addCucumberPreprocessorPlugin(on, config);
+
+    on(
+        "file:preprocessor",
+        browserify(config, {
+            typescript: require.resolve("typescript"),
+        })
+    );
+
+    return config;
+}
+
+export default defineConfig({
+    e2e: {
+        baseUrl: "https://google.com",
+        specPattern: "**/*.feature",
+        setupNodeEvents,
+    },
+
+});
---
+++ cypress/support/step_definitions/au.spec.ts
@@ -0,0 +1,13 @@
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When("I visit google.com", () => {
+	cy.visit("https://www.google.com/");
+});
+
+Then("I should see a search bar", () => {
+	cy.get('#APjFqb').should('exist');
+});
+
+Then("Start to type your And step here", () => {
+	cy.get('#APjFqb').type('ts5+cucumber+cy latest {enter}');
+});

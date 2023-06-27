1c93034996085afbe7f6da3d6fba0633a380fb84
“kaka5277” <254140626@qq.com>
2023-06-27 14:03:17 +1000

    type force true

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -49,7 +49,7 @@ Cypress.Commands.add('typeAtext', (field, text) => {
 })
 
 Cypress.Commands.add('passenger_select',(element,number)=>{
-    cy.get(element).clear().type(number)
+    cy.get(element).clear().type(number,{ force: true })
 })
 
 Cypress.Commands.add('selectValue',(field,value)=>{

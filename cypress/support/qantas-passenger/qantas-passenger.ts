ce68e67a2b171ac5df6f247f62ae6aa81a2a7068
“kaka5277” <254140626@qq.com>
2023-06-27 13:37:14 +1000

    fix:scrollto before click

--- cypress/support/qantas-passenger/qantas-passenger.ts
+++ cypress/support/qantas-passenger/qantas-passenger.ts
@@ -1,23 +1,22 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor"
 
 Given(/^I am on the flight search page$/, () => {
-	cy.visit('https://www.qantas.com/au/en/book-a-trip/flights.html')
+	 cy.visit('https://www.qantas.com/au/en/book-a-trip/flights.html')
 })
 
 Given("I select passengers as {string}", (passengers:string) => {
-    const n=passengers.split('-')
-	//cy.get('.css-tpxo3f-runway-popup-field__button').click()
-    cy.scrollTo(0,500)
-   // cy.clickElement('.css-tpxo3f-runway-popup-field__button')
-    //cy.passenger_select('input#adults',`${n[0]}`)
-   // cy.passenger_select('input#children',`${n[1]}`)
-   // cy.passenger_select('input#infants',`${n[2]}`)
+      const n=passengers.split('-')
+      cy.scrollTo(0,500)
+      cy.clickElement('.css-tpxo3f-runway-popup-field__button')
+      cy.passenger_select('input#adults',`${n[0]}`)
+      cy.passenger_select('input#children',`${n[1]}`)
+      cy.passenger_select('input#infants',`${n[2]}`)
 })
 
 When("I click '+' button in {string}", (row) => {
-	//cy.clickElement(`[data-testid=${row}] [aria-label="Increase Value"]`)
+	  cy.clickElement(`[data-testid=${row}] [aria-label="Increase Value"]`)
 })
 
 Then(/^I should see alert$/, () => {
-	//cy.contains('Only 9 passengers can be booked at a time.')
+	  cy.contains('Only 9 passengers can be booked at a time.')
 })
